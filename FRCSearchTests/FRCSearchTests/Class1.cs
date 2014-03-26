9b2d9ef088da1cb5b092c5f67a2ee58c85846b4d
jonathanthomson <jonathan.thomson@forrent.com>
2014-03-26 10:00:30 -0500

    Initial Setup

---
+++ FRCSearchTests/FRCSearchTests/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+using NUnit.Framework;
+
+namespace FRCSearchTests
+{
+	public class Class1
+	{
+		public IWebDriver driver;
+		[SetUp]
+		public void setup()
+		{
+			driver = new FirefoxDriver();
+		}
+		[TearDown]
+		public void Teardown()
+		{
+			driver.Quit();
+		}
+		[TestCase]
+		public void MyFRCTest1(string MyFRCTest1)
+		{
+			driver.Navigate().GoToUrl("http://www.forrent.com");
+			System.Threading.Thread.Sleep(500);
+
+			driver.FindElement(By.Id("cityState")).SendKeys("Valencia Park");
+			System.Threading.Thread.Sleep(500);
+
+			driver.FindElement(By.Id("searchMatch0-hcard")).Click();
+			System.Threading.Thread.Sleep(500);
+
+			driver.FindElement(By.XPath("//div[@class='bvMediaTrayIcon']"));
+
+
+			Assert.True(driver.Url.Equals("http://thumbs.frmonline.com/imgs/fr/propertyFiles/408/000/1/resized/01_138514015508240950142008275001000.jpg"));
+
+		}
+	}
+}
---
+++ MyGoogleSearchTest/MyGoogleSearchTest/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using NUnit.Framework;
+
+namespace MyGoogleSearchTest
+{
+    public class Class1
+    {
+		public IWebDriver driver;
+	[SetUp]
+	public void setup()
+	{
+		driver = new ChromeDriver();
+	}
+	[TearDown]
+	public void Teardown()
+	{
+		driver.Quit();
+	}
+	//[TestCase("Gmail - Email from Google")]  //searchString = Google
+	//public void MyGoogleTest(string MyGoogleTest)
+	//{
+	//	driver.Navigate().GoToUrl("http://www.google.com");
+	//	System.Threading.Thread.Sleep(500);
+	//	driver.FindElement(By.XPath("//a[@href='https://mail.google.com/mail/?tab=wm']")).Click();
+	//	System.Threading.Thread.Sleep(500);
+
+	//	Assert.True(driver.Title.Contains("Gmail - Email from Google"));
+	//}
+	[TestCase("charlie bit me - Google Search")]
+	public void MyGoogleTest2(string MyGoogleTest2)
+	{
+		driver.Navigate().GoToUrl("http://www.google.com");
+		System.Threading.Thread.Sleep(500);
+		driver.FindElement(By.Id("gbqfq")).SendKeys("Charlie Bit Me");
+		System.Threading.Thread.Sleep(500);
+		driver.FindElement(By.Id("gsr"));
+
+		Assert.True(driver.Title.Contains("Google"));
+
+	}
+
+
+
+
+    }
+}
